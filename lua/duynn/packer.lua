2a691eb8b463a4c2b19ce99fba0c5dcbab7bd83f
duynn <duynn@example.com>
2022-08-28 22:42:31 +0700

    add nvim-tree

--- lua/duynn/packer.lua
+++ lua/duynn/packer.lua
@@ -62,6 +62,13 @@ return require('packer').startup(function(use)
 
   -- Post-install/update hook with neovim command
   use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+  use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
