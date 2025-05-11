5dacf98439224e099819837bf543c653e368ae82
duynn <hotboyarsenal@gmail.com>
2025-05-11 19:07:07 +0700

    update copilot chat config

--- after/plugin/copilot-chat.lua
+++ after/plugin/copilot-chat.lua
@@ -3,7 +3,8 @@ local chat = require("CopilotChat")
 chat.setup({
   model="gemini-2.5-pro",
   window = {
-    layout = 'float', -- 'vertical', 'horizontal', 'float', 'replace'
+    temperature = 0.1,
+    layout = 'vertical', -- 'vertical', 'horizontal', 'float', 'replace'
     width = 0.5, -- fractional width of parent, or absolute width in columns when > 1
     height = 0.9, -- fractional height of parent, or absolute height in rows when > 1
     -- Options below only apply to floating windows
@@ -15,5 +16,4 @@ chat.setup({
     footer = nil, -- footer of chat window
     zindex = 1, -- determines if window is on top or below other floating windows
   },
-
 })
